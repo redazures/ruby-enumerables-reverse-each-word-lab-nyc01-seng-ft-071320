1679318835b956aed31176969894b13065eab4a1
james wu <jwu_nyc@yahoo.com>
2020-06-28 20:13:15 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,23 @@
+def reverse_each_word2(string)
+  array=string.split(" ")
+  ora=[]
+  array.each do |word|
+    ora<<word.reverse
+  end
+ora.join(" ")
+end
+
+
+def reverse_each_word(string)
+  array=string.split(" ")
+  array.collect {|word| word.reverse}.join(" ")
+
+end
+
+string="this is also a test"
+# array=string.split(" ")
+# array.collect do |word|
+#   ora<<word.reverse
+# end
+# puts ora
+ p reverse_each_word(string)
